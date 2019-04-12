be1bc3489bc00001fab505cf1c9b6a02a50149b7
AllBeNoes <allbenoeso.com>
2019-04-12 16:48:12 +0300

    Welcome to the family, son

--- test/filter/index.js
+++ test/filter/index.js
@@ -2,14 +2,60 @@ const _ = require('lodash');
 
 module.exports = {
     filterFavoriteFruit(data, favoriteFruit) {
-    return _.chain(data)
-        .filter((item) => {
-            return item.favoriteFruit === favoriteFruit;
-        })
-        .map((item) => {
+        return _.chain(data)
+            .filter((item) => {
+                return item.favoriteFruit === favoriteFruit;
+            })
+            .map((item) => {
                 return item.name;
-        })
-        .value();
+            })
+            .value();
+    },
+
+    filterSex(data) {
+        const male = _.chain(data)
+            .filter((male) => {
+                return male.gender === "male";
+            })
+            .map((male) => {
+                return male.name;
+            })
+            .value();
+        const female = _.chain(data)
+            .filter((female) => {
+                return female.gender === "female";
+            })
+            .map((female) => {
+                return female.name;
+            })
+            .value();
+        return {male: male, female: female}
+    },
+
+    filterTag(data) {
+        return _.chain(data)
+            .map((tag) => {
+                return tag.tags;
+            })
+            .flatten(data)
+            .uniq(data)
+            .value();
+
+    },
+
+    countTag(data) {
+        const res = _.chain(data)
+            .map((tag) => {
+                return tag.tags;
+            })
+            .flatten(data)
+            .value();
+        return _.chain(res)
+            .reduce(function (counts, word) {
+                counts[word] = (counts[word] || 0) + 1;
+                return counts;
+            }, {})
+            .value();
     }
 
 };
--- test/filter/index.test.js
+++ test/filter/index.test.js
@@ -1,11 +1,11 @@
 const expect = require('expect');
-const { filterFavoriteFruit } = require('./index');
+const _ = require('./index');
 const data = require('../data.json');
 
 
 describe('favoriteFruit', () => {
     it('should find bananas', () => {
-        const res = filterFavoriteFruit(data, 'banana');
+        const res = _.filterFavoriteFruit(data, 'banana');
         expect(res).toEqual([
             "Garrett Hester",
             "Elaine Cooke",
@@ -16,7 +16,7 @@ describe('favoriteFruit', () => {
     });
 
     it('should find strawberry', () => {
-        const res = filterFavoriteFruit(data, 'strawberry');
+        const res = _.filterFavoriteFruit(data, 'strawberry');
         expect(res).toEqual([
             "Foley Wong",
             "Hazel Lambert",
@@ -24,7 +24,161 @@ describe('favoriteFruit', () => {
             "Ann Hull",
             "Watts Newton",
             "Lenora Stanton"
-            ])
+        ])
     })
+});
 
+describe('SEX', () => {
+    it('should divide by sex', () => {
+        const res = _.filterSex(data);
+        expect(res).toEqual({
+            male:
+                [
+                    'Clemons Love',
+                    'Foley Wong',
+                    'Garrett Hester',
+                    'Valdez Compton',
+                    'Jarvis Melendez',
+                    'Watts Newton',
+                    'Hancock Sims'
+                ],
+            female:
+                [
+                    'Elaine Cooke',
+                    'Hazel Lambert',
+                    'June Watts',
+                    'Juanita Kaufman',
+                    'Kitty Hart',
+                    'Ann Hull',
+                    'Lenora Stanton',
+                    'Denise Curtis',
+                    'Mariana Christensen',
+                    'Shirley Murphy'
+                ]
+        })
+    });
+});
+
+describe('TAGS', () => {
+    it('should find tags', () => {
+        const res = _.filterTag(data);
+        expect(res).toEqual(
+            [
+
+                'nisi',
+                'occaecat',
+                'minim',
+                'velit',
+                'in',
+                'culpa',
+                'laboris',
+                'consequat',
+                'non',
+                'do',
+                'fugiat',
+                'irure',
+                'laborum',
+                'nostrud',
+                'incididunt',
+                'adipisicing',
+                'enim',
+                'esse',
+                'ex',
+                'commodo',
+                'pariatur',
+                'reprehenderit',
+                'officia',
+                'labore',
+                'tempor',
+                'eiusmod',
+                'exercitation',
+                'duis',
+                'sit',
+                'ut',
+                'qui',
+                'ea',
+                'aute',
+                'mollit',
+                'eu',
+                'sunt',
+                'est',
+                'sint',
+                'excepteur',
+                'dolore',
+                'ad',
+                'aliquip',
+                'magna',
+                'et',
+                'anim',
+                'consectetur',
+                'nulla',
+                'veniam',
+                'cillum',
+                'Lorem',
+                'amet',
+                'quis'
+            ]
+        )
+    })
+});
+
+describe('TAGS NUMBER', () => {
+    it('should count tags', () => {
+        const res = _.countTag(data);
+        expect(res).toEqual(
+            {
+                nisi: 1,
+                occaecat: 3,
+                minim: 1,
+                velit: 5,
+                in: 2,
+                culpa: 3,
+                laboris: 2,
+                consequat: 4,
+                non: 2,
+                do: 5,
+                fugiat: 6,
+                irure: 3,
+                laborum: 1,
+                nostrud: 3,
+                incididunt: 1,
+                adipisicing: 4,
+                enim: 1,
+                esse: 2,
+                ex: 2,
+                commodo: 3,
+                pariatur: 4,
+                reprehenderit: 5,
+                officia: 1,
+                labore: 2,
+                tempor: 3,
+                eiusmod: 3,
+                exercitation: 1,
+                duis: 2,
+                sit: 2,
+                ut: 5,
+                qui: 5,
+                ea: 2,
+                aute: 3,
+                mollit: 1,
+                eu: 1,
+                sunt: 2,
+                est: 2,
+                sint: 2,
+                excepteur: 1,
+                dolore: 2,
+                ad: 1,
+                aliquip: 3,
+                magna: 1,
+                et: 1,
+                anim: 3,
+                consectetur: 1,
+                nulla: 1,
+                veniam: 1,
+                cillum: 1,
+                Lorem: 1,
+                amet: 1,
+                quis: 1
+            });
+    })
 });
