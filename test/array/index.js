953686c2e3aa186b525a361daaabefd9127ad4e4
AllBeNoes <allbenoeso.com>
2019-04-16 17:56:47 +0300

    Building

--- test/array/index.js
+++ test/array/index.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const math = require('mathjs');
 
 module.exports = {
     table(maxX, minX, maxY, minY) {
@@ -11,31 +12,69 @@ module.exports = {
         for (let y = minY; y < maxY + 1; y++) {
             const row = [y];
             for (let x = minX; x < maxX + 1; x++) {
-                const mult = x*y;
+                const mult = x * y;
                 row.push(mult);
             }
             res.push(row);
         }
         return res
     },
-    print (array, draw) {
+    print(array, draw) {
         for (let i = 0; i < array.length; i++) {
             const row = array[i];
             draw(row.join("\t\t"));
         }
+    },
+
+    bldng(entrences, flors, roomsPerFlor) {
+        const res = [];
+        const rpe = roomsPerFlor * flors;
+        const rpb = rpe * entrences;
+
+        for (let i = 0; i < flors; i++) {
+            res.push([])
+        }
+
+        for (let i = 0; i < rpb; i++) {
+            res[(math.abs((~~((i % rpe) / roomsPerFlor)) - (flors - 1)))].push(i + 1)
+        }
+
+
+        return res;
     }
- };
 
 
+};
+
+// for (let i = 0; i < flors*enterences + 1; i++) {
+//     const row = [y];
+//     for (let x = minX; x < maxX + 1; x++) {
+//         const mult = x*y;
+//         row.push(mult);
+//     }
+//     res.push(row);
+// }
+// return res
+//
+//
+// 1  2  3|16 17 18
+// 4  5  6|19 20 21
+// 7  8  9|22 23 24
+// 10 11 12|25 26 27
+// 13 14 15|28 29 30
+//
+//     [[1, 2, 3, 16, 17, 18],
+//     [4, 5, 6, 19, 20, 21],
+//     [7, 8, 9, 22, 23, 24],
+//     [10, 11, 12, 25, 26, 27],
+//     [13, 14, 15, 28, 29, 30]]
+//
+// 5 fl, 2 ent, 3 RPF
+//
+//
 //        add  remove  start  end
 // push    X                   X
 // pop           X             X
 // unshift X             X
 // shift         X       X
 
-// .filter((item) => {
-//     return item.favoriteFruit === favoriteFruit;
-// })
-//     .map((item) => {
-//         return item.name;
-//     })
--- test/array/index.test.js
+++ test/array/index.test.js
@@ -4,10 +4,47 @@ const utils = require('./index');
 
 describe('MULTIPLICATION TABLE', () => {
     it('should make multiplication table', () => {
-        const res = utils.table(3,1,3,1);
+        const res = utils.table(3, 1, 3, 1);
         utils.print(res, console.log);
-        // expect(res).toEqual([
-        //
-        // ]);
+        expect(res).toEqual([
+            [
+                "",
+                1,
+                2,
+                3,
+            ],
+            [
+                1,
+                1,
+                2,
+                3
+            ],
+            [
+                2,
+                2,
+                4,
+                6
+            ],
+            [
+                3,
+                3,
+                6,
+                9
+            ],
+        ]);
+    });
+});
+
+describe('BUILDING', () => {
+    it('should make building table', () => {
+        const res = utils.bldng(2, 5, 3);
+        utils.print(res, console.log);
+        expect(res).toEqual([
+            [ 13, 14, 15, 28, 29, 30 ],
+            [ 10, 11, 12, 25, 26, 27 ],
+            [ 7, 8, 9, 22, 23, 24 ],
+            [ 4, 5, 6, 19, 20, 21 ],
+            [ 1, 2, 3, 16, 17, 18 ]
+        ])
     });
 });
